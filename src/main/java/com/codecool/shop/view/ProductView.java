7701071418f125f6aeca66e103ab7ab905dd7def
mari <maristein3@gmail.com>
2017-05-02 13:32:09 +0200

    Add package view

---
+++ src/main/java/com/codecool/shop/view/ProductView.java
@@ -0,0 +1,17 @@
+package com.codecool.shop.view;
+
+import com.codecool.shop.model.Product;
+
+import java.util.List;
+
+/**
+ * Created by mar on 02.05.17.
+ */
+public class ProductView {
+
+    public void displayView(List<Product> products) {
+        for(Product p: products) {
+            System.out.println(p.getName());
+        }
+    }
+}
