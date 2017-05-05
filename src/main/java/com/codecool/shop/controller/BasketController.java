9d7a231842a105f9994c680484bdad7ba62a19cf
pawellasota <plasota76@gmail.com>
2017-05-05 09:16:59 +0200

    add basket controller

---
+++ src/main/java/com/codecool/shop/controller/BasketController.java
@@ -0,0 +1,10 @@
+package com.codecool.shop.controller;
+
+/**
+ * Created by pawel on 05.05.17.
+ */
+public class BasketController {
+    public void addToCartAction(){
+        System.out.println("Added to cart method");
+    }
+}
