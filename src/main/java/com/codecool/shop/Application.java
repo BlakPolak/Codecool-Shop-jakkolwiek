350756733cc0ff6d6cb74a9dacf367625b55baf5
Anna Matras <ania.matras@gmail.com>
2017-05-16 13:32:03 +0000

    Code refactor

--- src/main/java/com/codecool/shop/Application.java
+++ src/main/java/com/codecool/shop/Application.java
@@ -1,8 +1,6 @@
 
 package com.codecool.shop;
 
-import com.codecool.shop.controller.MainMenuController;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -28,10 +26,5 @@ public class Application {
     private void connectToDb() throws SQLException {
         System.out.println("Connection to DB...");
         this.connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/database.db");
-    }
-
-    private void dispatchMainMenuController() {
-        MainMenuController mainMenuController = new MainMenuController();
-        mainMenuController.mainMenuAction();
     }
 }
