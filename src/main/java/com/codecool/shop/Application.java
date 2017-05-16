cd4d1d1d031ebb32809ba9b0f3fd4b6329733d42
pawellasota <plasota76@gmail.com>
2017-05-16 12:03:45 +0200

    Remove all methods associated with terminal views

--- src/main/java/com/codecool/shop/Application.java
+++ src/main/java/com/codecool/shop/Application.java
@@ -1,15 +1,17 @@
 
 package com.codecool.shop;
 
-import com.codecool.shop.controller.MainMenuController;
+import com.codecool.shop.model.Basket;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Application {
     private static final Application app = new Application();
     private Connection connection;
+    private Basket basket = new Basket();
 
     public Application() {
     Routes routes = new Routes();
@@ -25,13 +27,13 @@ public class Application {
 
     }
 
+    public Basket getBasket() {
+        return basket;
+    }
+
     private void connectToDb() throws SQLException {
         System.out.println("Connection to DB...");
         this.connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/database.db");
     }
 
-    private void dispatchMainMenuController() {
-        MainMenuController mainMenuController = new MainMenuController();
-        mainMenuController.mainMenuAction();
-    }
 }
--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -4,8 +4,6 @@ import com.codecool.shop.dao.*;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
-import com.codecool.shop.view.ProductView;
-import com.codecool.shop.view.UserInput;
 import spark.ModelAndView;
 
 import java.util.ArrayList;
@@ -17,7 +15,6 @@ public class ProductController {
     private ProductDao productDao = new ProductDaoSqlite();
     private ProductCategoryDao productCategoryDao = new ProductCategoryDaoSqlite();
     private ProductSupplierDao productSupplierDao = new ProductSupplierDaoSqlite();
-    private ProductView view = new ProductView();
 
     public ModelAndView listProducts() {
         List<Product> products = productDao.getAll();
@@ -28,21 +25,19 @@ public class ProductController {
 
     public void listProductsByCategory() {
         List<ProductCategory> categories = productCategoryDao.getAll();
-        view.displayCategoriesList(categories);
 
-        Integer categoryId = UserInput.getUserInput();
-        ProductCategory category = productCategoryDao.find(categoryId);
-        List<Product> products = productDao.getBy(category);
-        view.displayProductsList(products);
+//        Integer categoryId = UserInput.getUserInput();
+//        ProductCategory category = productCategoryDao.find(categoryId);
+//        List<Product> products = productDao.getBy(category);
+//        view.displayProductsList(products);
     }
 
     public void listProductsBySupplier() {
         List<Supplier> suppliers = productSupplierDao.getAll();
-        view.displaySuppliersList(suppliers);
 
-        Integer supplierId = UserInput.getUserInput();
-        Supplier supplier = productSupplierDao.find(supplierId);
-        List<Product> products = productDao.getBy(supplier);
-        view.displayProductsList(products);
+//        Integer supplierId = UserInput.getUserInput();
+//        Supplier supplier = productSupplierDao.find(supplierId);
+//        List<Product> products = productDao.getBy(supplier);
+//        view.displayProductsList(products);
     }
 }
