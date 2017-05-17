0cc84bbe0c29520c9b783276d7a2ba348ec95422
mari <maristein3@gmail.com>
2017-05-17 15:21:50 +0200

    Create route to display basket and add to basket

--- src/main/java/com/codecool/shop/Routes.java
+++ src/main/java/com/codecool/shop/Routes.java
@@ -1,22 +1,31 @@
 package com.codecool.shop;
 
 
+import com.codecool.shop.controller.BasketController;
 import com.codecool.shop.controller.ProductController;
+import com.codecool.shop.model.Basket;
 import spark.Request;
 import spark.Response;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
-import static spark.Spark.get;
-import static spark.Spark.port;
-import static spark.Spark.staticFileLocation;
+import static spark.Spark.*;
 
 public class Routes {
     ProductController productController = new ProductController();
+    BasketController basketController = new BasketController();
     void run() {
         staticFileLocation("/public");
         port(8888);
         get("/", (Request req, Response res) -> {
             return new ThymeleafTemplateEngine().render(productController.listProducts());
+        });
+
+        post("/add-to-basket/", (Request req, Response res) -> {
+            return new ThymeleafTemplateEngine().render(basketController.addToCartAction(req,res));
+        });
+
+        get("/basket/", (Request req, Response res) -> {
+            return new ThymeleafTemplateEngine().render(basketController.listProductsInBasket());
         });
     }
 
