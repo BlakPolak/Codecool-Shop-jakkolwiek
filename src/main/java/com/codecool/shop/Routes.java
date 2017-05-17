71e6bbbd5043198973eabd1fd7cf7d46c465f2cf
Pawel Polakiewicz <pb.polakiewicz@gmail.com>
2017-05-17 12:24:18 +0200

    Add /supplier route

--- src/main/java/com/codecool/shop/Routes.java
+++ src/main/java/com/codecool/shop/Routes.java
@@ -6,17 +6,20 @@ import spark.Request;
 import spark.Response;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
-import static spark.Spark.get;
-import static spark.Spark.port;
-import static spark.Spark.staticFileLocation;
+import static spark.Spark.*;
 
 public class Routes {
     ProductController productController = new ProductController();
     void run() {
+        exception(Exception.class, (e, req, res) -> e.printStackTrace());
         staticFileLocation("/public");
         port(8888);
         get("/", (Request req, Response res) -> {
             return new ThymeleafTemplateEngine().render(productController.listProducts());
+        });
+
+        get("/supplier", (Request req, Response res) -> {
+            return new ThymeleafTemplateEngine().render(productController.listProductsBySupplier(req, res));
         });
     }
 
