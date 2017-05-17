4898df8a031d29f63273bed72323b4c7c6526665
Anna Matras <ania.matras@gmail.com>
2017-05-17 11:24:06 +0000

    Add route to render categories

--- src/main/java/com/codecool/shop/Routes.java
+++ src/main/java/com/codecool/shop/Routes.java
@@ -6,18 +6,25 @@ import spark.Request;
 import spark.Response;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
-import static spark.Spark.get;
-import static spark.Spark.port;
-import static spark.Spark.staticFileLocation;
+import static spark.Spark.*;
 
 public class Routes {
     ProductController productController = new ProductController();
     void run() {
+        exception(Exception.class, (e, req, res) -> e.printStackTrace());
         staticFileLocation("/public");
         port(8888);
         get("/", (Request req, Response res) -> {
             return new ThymeleafTemplateEngine().render(productController.listProducts());
         });
+
+        get("/category", (req, res) -> {
+            productController.listProductsByCategory(req, res);
+            res.redirect("/");
+            return "";
+        });
+
+
     }
 
 }
