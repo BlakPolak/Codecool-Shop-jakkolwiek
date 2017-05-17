02995a523902e08dc81c595eab92946941ff15d7
mari <maristein3@gmail.com>
2017-05-17 15:26:40 +0200

    Remove unused methods

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.*;
+import com.codecool.shop.model.Basket;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
@@ -16,28 +17,4 @@ public class ProductController {
     private ProductCategoryDao productCategoryDao = new ProductCategoryDaoSqlite();
     private ProductSupplierDao productSupplierDao = new ProductSupplierDaoSqlite();
 
-    public ModelAndView listProducts() {
-        List<Product> products = productDao.getAll();
-        Map<String, Object> model = new HashMap<>();
-        model.put("products", products);
-        return new ModelAndView(model, "product/index");
-    }
-
-    public void listProductsByCategory() {
-        List<ProductCategory> categories = productCategoryDao.getAll();
-
-//        Integer categoryId = UserInput.getUserInput();
-//        ProductCategory category = productCategoryDao.find(categoryId);
-//        List<Product> products = productDao.getBy(category);
-//        view.displayProductsList(products);
-    }
-
-    public void listProductsBySupplier() {
-        List<Supplier> suppliers = productSupplierDao.getAll();
-
-//        Integer supplierId = UserInput.getUserInput();
-//        Supplier supplier = productSupplierDao.find(supplierId);
-//        List<Product> products = productDao.getBy(supplier);
-//        view.displayProductsList(products);
-    }
 }
