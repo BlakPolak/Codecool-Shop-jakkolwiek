7aad773179d5c01afb293b560800f0ed504d02d2
pawellasota <plasota76@gmail.com>
2017-05-17 15:48:53 +0200

    Remove unused routes

--- src/main/java/com/codecool/shop/Routes.java
+++ src/main/java/com/codecool/shop/Routes.java
@@ -5,7 +5,6 @@ import spark.Response;
 import static spark.Spark.get;
 import static spark.Spark.port;
 import static spark.Spark.staticFileLocation;
-import spark.template.thymeleaf.ThymeleafTemplateEngine;
 import static spark.Spark.*;
 
 public class Routes {
@@ -16,14 +15,7 @@ public class Routes {
         port(8888);
 
         get("/", (Request req, Response res) -> {
-            return Application.getApp().getProductController().listProducts();
-        });
-        get("/category", (Request req, Response res) -> {
-            return new ThymeleafTemplateEngine().render(Application.getApp().getProductController().listProductsByCategory(req, res));
-        });
-
-        get("/supplier", (Request req, Response res) -> {
-            return new ThymeleafTemplateEngine().render(productController.listProductsBySupplier(req, res));
+            return Application.getApp().getProductController().listProducts(req, res);
         });
     }
 }
