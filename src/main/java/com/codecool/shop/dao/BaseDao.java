ab16ef499476994d9d3e46a6aa7b420b194d6714
Anna Matras <ania.matras@gmail.com>
2017-05-17 13:27:38 +0000

    Refactor abstract class

--- src/main/java/com/codecool/shop/dao/BaseDao.java
+++ src/main/java/com/codecool/shop/dao/BaseDao.java
@@ -1,20 +1,12 @@
 package com.codecool.shop.dao;
 
+import com.codecool.shop.Application;
+
 import java.sql.Connection;
 
 abstract class BaseDao {
-    private Connection connection;
-
-    public BaseDao() {
-        Connection connection = SqliteJDBCConnector.connection();
-        this.setConnection(connection);
-    }
-
-    public void setConnection(Connection connection) {
-        this.connection = connection;
-    }
 
     public Connection getConnection() {
-        return connection;
+        return Application.getApp().getConnection();
     }
 }
