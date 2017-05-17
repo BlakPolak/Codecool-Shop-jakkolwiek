408b59493c5d76d1b5aeb1558968b5a9023f9ff9
mari <maristein3@gmail.com>
2017-05-17 19:12:53 +0200

    Remove unnecessary prints and imports

--- src/main/java/com/codecool/shop/controller/BasketController.java
+++ src/main/java/com/codecool/shop/controller/BasketController.java
@@ -3,12 +3,8 @@ package com.codecool.shop.controller;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.ProductDaoSqlite;
 import com.codecool.shop.model.*;
-import com.sun.org.apache.regexp.internal.RE;
-import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,15 +15,14 @@ public class BasketController extends  BaseController{
 
     public String addToCartAction(Request req, Response res) {
         int id = Integer.parseInt(req.queryParams("id"));
-        List<Product> products = productDao.getById(id);
-        basket.add(products.get(0), 1);
+        Product product = productDao.find(id);
+        basket.add(product, 1);
         res.redirect("/basket/");
         return "";
     }
 
     public String listProductsInBasket() {
         List<BasketItem> products = basket.getItems();
-        System.out.println(products.get(0).getProduct());
         Map<String, Object> model = new HashMap<>();
         model.put("products", products);
         String templatePath = "product/basket";
