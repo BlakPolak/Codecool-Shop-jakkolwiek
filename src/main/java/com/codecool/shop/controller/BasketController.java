288f534ee031e8db1e3c5d729d04d61101931dde
Pawel Polakiewicz <pb.polakiewicz@gmail.com>
2017-05-17 12:30:32 +0200

    Resolves conflicts

--- src/main/java/com/codecool/shop/controller/BasketController.java
+++ src/main/java/com/codecool/shop/controller/BasketController.java
@@ -12,11 +12,12 @@ public class BasketController {
     Basket basket = new Basket();
 
     public void addToCartAction() {
-        List<Product> products = this.productDao.getAll();
-
+//        List<Product> products = this.productDao.getAll();
+//        this.productView.displayProductsList(products);
 //        System.out.print("Select product by giving id: ");
 //        Integer productId = UserInput.getUserInput();
 //        Product product = productDao.find(productId);
 //        this.basket.add(product, 1);
+//        this.basketView.displayBasketItems(this.basket.getItems());
     }
 }
