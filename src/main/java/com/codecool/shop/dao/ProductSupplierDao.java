3f1e5c24afb4b57078c62b2d9bead0532f71a675
pawellasota <plasota76@gmail.com>
2017-05-18 15:53:24 +0200

    Add throwing SQLExceptions to methods declarations

--- src/main/java/com/codecool/shop/dao/ProductSupplierDao.java
+++ src/main/java/com/codecool/shop/dao/ProductSupplierDao.java
@@ -3,14 +3,13 @@ package com.codecool.shop.dao;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface ProductSupplierDao {
 
-    void add(Supplier category);
-    Supplier find(int id);
-    void remove(int id);
-
-    List<Supplier> getAll();
-
+    void add(Supplier category) throws SQLException;
+    Supplier find(Integer id) throws SQLException;
+    void remove(Integer id) throws SQLException;
+    List<Supplier> getAll() throws SQLException;
 }
