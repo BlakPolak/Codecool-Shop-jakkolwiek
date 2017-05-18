99471ebd1e47650877d5e936b9b735fc91ef2abe
pawellasota <plasota76@gmail.com>
2017-05-18 22:01:23 +0200

    Remove unused methods

--- src/main/java/com/codecool/shop/dao/ProductSupplierDao.java
+++ src/main/java/com/codecool/shop/dao/ProductSupplierDao.java
@@ -1,15 +1,11 @@
 package com.codecool.shop.dao;
 
-import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
-
 import java.sql.SQLException;
 import java.util.List;
 
 public interface ProductSupplierDao {
 
-    void add(Supplier category) throws SQLException;
-    Supplier find(Integer id) throws SQLException;
-    void remove(Integer id) throws SQLException;
+    Supplier getBy(Integer id) throws SQLException;
     List<Supplier> getAll() throws SQLException;
 }
