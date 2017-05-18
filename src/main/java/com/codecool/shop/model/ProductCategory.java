251b52bf42b63c3196f9376827b796501b4a3e05
pawellasota <plasota76@gmail.com>
2017-05-18 15:47:25 +0200

    Remove unused methods, correct constructor

--- src/main/java/com/codecool/shop/model/ProductCategory.java
+++ src/main/java/com/codecool/shop/model/ProductCategory.java
@@ -1,35 +1,16 @@
 package com.codecool.shop.model;
 
-import java.util.ArrayList;
 
 public class ProductCategory extends BaseModel {
     private String department;
-    private ArrayList<Product> products;
 
     public ProductCategory(String name, String department, String description) {
-        super(name);
-        this.department = department;
-        this.products = new ArrayList<>();
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
+        super(name, description);
         this.department = department;
     }
 
-    public void setProducts(ArrayList<Product> products) {
-        this.products = products;
-    }
-
-    public ArrayList getProducts() {
-        return this.products;
-    }
-
-    public void addProduct(Product product) {
-        this.products.add(product);
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String toString() {
