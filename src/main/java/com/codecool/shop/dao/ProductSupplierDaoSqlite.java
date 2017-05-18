1bdad898fafad98897485d62565a1fa3a684c317
pawellasota <plasota76@gmail.com>
2017-05-18 22:02:23 +0200

    Add getSuppliers(), refactor find() and change it to getBy()

--- src/main/java/com/codecool/shop/dao/ProductSupplierDaoSqlite.java
+++ src/main/java/com/codecool/shop/dao/ProductSupplierDaoSqlite.java
@@ -1,51 +1,36 @@
 package com.codecool.shop.dao;
 
 import com.codecool.shop.model.Supplier;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProductSupplierDaoSqlite extends BaseDao implements ProductSupplierDao {
 
-    @Override
-    public void add(Supplier category) throws SQLException {
-
-    }
-
-    @Override
-    public Supplier find(Integer id) throws SQLException {
-        Supplier supplier = null;
-        Statement statement = this.getConnection().createStatement();
-        ResultSet rs = statement.executeQuery("select * from suppliers where id = " + Integer.toString(id));
-        if(rs.next()) {
-            supplier = new Supplier(
-                    rs.getString("name"),
-                    rs.getString("description")
-            );
-            supplier.setId(rs.getInt("id"));
-        }
-        return supplier;
-    }
-
-    @Override
-    public void remove(Integer id) throws SQLException {
-
+     @Override
+    public Supplier getBy(Integer id) throws SQLException {
+        PreparedStatement statement = this.getConnection().prepareStatement("select * from suppliers where id = ?");
+        statement.setInt(1, id);
+        return this.getSuppliers(statement).get(0);
     }
 
     @Override
     public List<Supplier> getAll() throws SQLException {
+        PreparedStatement statement = this.getConnection().prepareStatement("select distinct * from suppliers");
+        return getSuppliers(statement);
+    }
+
+    private List<Supplier> getSuppliers(PreparedStatement statement) throws SQLException {
         List<Supplier> suppliers = new ArrayList<>();
-        Statement statement = this.getConnection().createStatement();
-        ResultSet rs = statement.executeQuery("SELECT DISTINCT * FROM suppliers");
+        ResultSet rs = statement.executeQuery();
         while(rs.next()) {
-            Supplier supplier = new Supplier(
+            suppliers.add(new Supplier(
+                    rs.getInt("id"),
                     rs.getString("name"),
                     rs.getString("description")
-            );
-            supplier.setId(rs.getInt("id"));
-            suppliers.add(supplier);
+            ));
         }
         return suppliers;
     }
