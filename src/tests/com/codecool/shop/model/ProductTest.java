70963749d994eec257708d26a86ff4e9fd09bf9c
castarox <mika.kamil12@gmail.com>
2017-05-29 14:44:13 +0200

    Implement testFailSetPriceLtZero

--- src/tests/com/codecool/shop/model/ProductTest.java
+++ src/tests/com/codecool/shop/model/ProductTest.java
@@ -1,7 +1,24 @@
 package com.codecool.shop.model;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTest {
+    @Test
+    void testFailSetPriceLtZero(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Product(0,
+                    "product",
+                    -5f,
+                    "PLN",
+                    "description",
+                    null,
+                    null
+            );
+        });
+    }
+
+
 
 }
