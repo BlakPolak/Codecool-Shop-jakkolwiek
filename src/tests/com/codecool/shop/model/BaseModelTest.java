80ea6ed7408ced5089310aaa690834df2dbbb5d7
cj <sigamateusz@gmail.com>
2017-05-29 12:14:02 +0200

    Add junit to Maven & add BaseModelTest

---
+++ src/tests/com/codecool/shop/model/BaseModelTest.java
@@ -0,0 +1,32 @@
+package com.codecool.shop.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BaseModelTest {
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void setDescription() {
+    }
+
+}
