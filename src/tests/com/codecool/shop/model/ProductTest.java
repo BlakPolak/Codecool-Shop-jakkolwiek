c2ff1eb6739119856654f062fdbf5e37cab83c25
castarox <mika.kamil12@gmail.com>
2017-05-30 12:13:17 +0200

    Create BeforeEach for creating product

--- src/tests/com/codecool/shop/model/ProductTest.java
+++ src/tests/com/codecool/shop/model/ProductTest.java
@@ -1,34 +1,36 @@
 package com.codecool.shop.model;
 
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTest {
+    private Product product;
+
+    @BeforeEach
+    void getProduct(){
+        product = new Product(0,
+                "product",
+                -5f,
+                "PLN",
+                "description",
+                null,
+                null
+        );
+    }
+
     @Test
     void testFailSetPriceLtZero(){
         assertThrows(IllegalArgumentException.class, () -> {
-            new Product(0,
-                    "product",
-                    -5f,
-                    "PLN",
-                    "description",
-                    null,
-                    null
-            );
+            product.setPrice(-5f, "pln");
         });
     }
 
     @Test
     void testFailIfPriceIsNotAccurate() {
-        Product product = new Product(null,
-                null,
-                12.99f,
-                "PLN",
-                null,
-                null,
-                null
-        );
+        product.setPrice(12.99f, "PLN");
         assertEquals(12.99f + " " + "PLN",product.getPrice());
     }
 
