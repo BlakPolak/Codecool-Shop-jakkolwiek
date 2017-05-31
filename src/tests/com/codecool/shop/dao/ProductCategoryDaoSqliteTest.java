83643dc65e63d333f9145f79010040261ca25911
Pati Mikulska <pp.mikulska@gmail.com>
2017-05-31 11:58:34 +0200

    Add mockito to maven, create epty klas for testing PCDaoSqlite

---
+++ src/tests/com/codecool/shop/dao/ProductCategoryDaoSqliteTest.java
@@ -0,0 +1,17 @@
+package com.codecool.shop.dao;
+
+import com.codecool.shop.Application;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+class ProductCategoryDaoSqliteTest {
+
+}
