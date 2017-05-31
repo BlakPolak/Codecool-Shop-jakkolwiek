ee5cf5aa7c84f3119dcebfa6b05d7eac94e0ef44
cj <sigamateusz@gmail.com>
2017-05-31 15:43:02 +0200

    Create set connection & productSupplierDao

--- src/tests/com/codecool/shop/dao/ProductSupplierDaoSqliteTest.java
+++ src/tests/com/codecool/shop/dao/ProductSupplierDaoSqliteTest.java
@@ -1,7 +1,24 @@
 package com.codecool.shop.dao;
 
+import com.codecool.shop.model.Supplier;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductSupplierDaoSqliteTest {
+    private Connection connection;
+    private ProductSupplierDao productSupplierDao;
 
+    @BeforeEach
+    void setConnectionToDBAndCreateDaoSqlite() throws SQLException {
+        SqliteJDBCConnector.setConnection("jdbc:sqlite:src/tests/test.db");
+        connection = SqliteJDBCConnector.getConnection();
+        productSupplierDao = new ProductSupplierDaoSqlite(connection);
+    }
 }
