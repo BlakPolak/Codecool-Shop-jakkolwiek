6b0dfd15368bd6bfcc73c79919bb28530d9083bc
castarox <mika.kamil12@gmail.com>
2017-05-31 15:12:43 +0200

    Add Dao objects to constructor

--- src/main/java/com/codecool/shop/dao/ProductDaoSqlite.java
+++ src/main/java/com/codecool/shop/dao/ProductDaoSqlite.java
@@ -4,6 +4,8 @@ import com.codecool.shop.Application;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
+
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,6 +14,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProductDaoSqlite extends BaseDao implements ProductDao {
+    ProductCategoryDao productCategoryDao;
+    ProductSupplierDao productSupplierDao;
+
+    public ProductDaoSqlite(Connection connection,
+                            ProductCategoryDao productCategoryDao,
+                            ProductSupplierDao productSupplierDao) {
+        super(connection);
+        this.productCategoryDao = productCategoryDao;
+        this.productSupplierDao = productSupplierDao;
+    }
 
     @Override
     public List<Product> getAll() throws SQLException {
@@ -58,8 +70,8 @@ public class ProductDaoSqlite extends BaseDao implements ProductDao {
                     rs.getFloat("price"),
                     "PLN",
                     rs.getString("description"),
-                    Application.getApp().getProductController().getProductCategoryDao().getBy(rs.getInt("category_id")),
-                    Application.getApp().getProductController().getProductSupplierDao().getBy(rs.getInt("supplier_id"))
+                    productCategoryDao.getBy(rs.getInt("category_id")),
+                    productSupplierDao.getBy(rs.getInt("supplier_id"))
             ));
         }
         return products;
