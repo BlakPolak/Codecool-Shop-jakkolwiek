8ad2533ae5b50cc0ae15f6b9ed7ac0c240dc56e5
castarox <mika.kamil12@gmail.com>
2017-05-31 16:32:42 +0200

    Implement testGetAllReturnCorrectSize and BeforeEach

--- src/tests/com/codecool/shop/dao/ProductDaoSqliteTest.java
+++ src/tests/com/codecool/shop/dao/ProductDaoSqliteTest.java
@@ -1,7 +1,38 @@
 package com.codecool.shop.dao;
 
+import com.codecool.shop.model.Supplier;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductDaoSqliteTest {
+    private Connection connection;
+    private ProductCategoryDao productCategoryDao;
+    private ProductSupplierDao productSupplierDao;
+    private ProductDao productDao;
 
+    @BeforeEach
+    void createDbConnection() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:sqlite:src/tests/test.db");
+        productCategoryDao = mock(ProductCategoryDaoSqlite.class);
+        when(productCategoryDao.getBy(anyInt())).thenReturn(null);
+        productSupplierDao = mock(ProductSupplierDaoSqlite.class);
+        when(productSupplierDao.getBy(anyInt())).thenReturn(null);
+        productDao = new ProductDaoSqlite(
+                connection,
+                productCategoryDao,
+                productSupplierDao
+        );
+    }
+
+    @Test
+    void testGetAllReturnCorrectSize() throws SQLException{
+        assertEquals(1,productDao.getAll().size());
+    }
 }
