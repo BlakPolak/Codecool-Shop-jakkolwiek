24e4469ecb215bd1407804382f7a5810af060a65
cj <sigamateusz@gmail.com>
2017-06-01 10:01:28 +0200

    Override equals() for ProductCategory

--- src/main/java/com/codecool/shop/model/ProductCategory.java
+++ src/main/java/com/codecool/shop/model/ProductCategory.java
@@ -20,4 +20,13 @@ public class ProductCategory extends BaseModel {
                 this.department,
                 this.description);
     }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || getClass() != other.getClass()) return false;
+        if (!super.equals(other)) return false;
+        ProductCategory that = (ProductCategory) other;
+        return department != null ? department.equals(that.department) : that.department == null;
+    }
 }
